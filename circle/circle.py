c61a31241f8d983394020409ac3d86cd218401cf
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-09-24 11:55:52 -0500

    Morsel circle -> Done

---
+++ circle/circle.py
@@ -0,0 +1,44 @@
+# Circle class
+
+from math import pi
+
+"""
+Be careful with naming convention of setters and getters
+having the samename of setter, getters, attributes will 
+raise a RunTimeError: Maximum recursion achieved
+"""
+
+class Circle:
+    def __init__(self, radius = 1):
+        self.radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError('Radius cannot be negative')
+        self._radius = radius
+
+    # getter - read only
+    @property
+    def diameter(self):
+        return self.radius * 2
+    
+    # setter - read/write
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2
+
+    @property
+    def area(self):
+        return self.radius * self.radius * pi
+
+    # string representation
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
+
+    def __str__(self):
+        return 'Circle({})'.format(self.radius)
