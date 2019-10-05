a49e06fe2b7e91bcaaa108c0138cb23287b31f65
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-05 02:24:57 -0500

    Update fix_csv.py
    Changed variable name

--- fix_csv/fix_csv.py
+++ fix_csv/fix_csv.py
@@ -12,14 +12,14 @@ parser.add_argument('--in-quote', type=str, help='specify input quote')
 args = parser.parse_args()
 
 # read csv
-with open(args.original_file, newline='') as of:
+with open(args.original_file, newline='') as original_file:
     if args.in_delimiter and args.in_quote:
-        rows = list(csv.reader(of, delimiter=args.in_delimiter, quotechar=args.in_quote))
+        rows = list(csv.reader(original_file, delimiter=args.in_delimiter, quotechar=args.in_quote))
     else:
-        dialect = csv.Sniffer().sniff(of.read())
-        of.seek(0)
-        rows = list(csv.reader(of, dialect=dialect))
+        dialect = csv.Sniffer().sniff(original_file.read())
+        original_file.seek(0)
+        rows = list(csv.reader(original_file, dialect=dialect))
 
 # write csv
-with open(args.fixed_file, mode='wt', newline='') as ff:
-    csv.writer(ff).writerows(rows)
+with open(args.fixed_file, mode='wt', newline='') as fix_file:
+    csv.writer(fix_file).writerows(rows)
