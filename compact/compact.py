9753b98c16a8cbf3eab3450c9d76e3daae930790
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-10-08 14:36:10 -0500

    Compact morsel -> Done

---
+++ compact/compact.py
@@ -0,0 +1,26 @@
+
+def compact1(iterable):
+    """
+    This works only for lists
+    """
+    if len(iterable) < 2:
+        return iterable
+    
+    return [iterable[e] for e in range(len(iterable)) if e == 0 or iterable[e] != iterable[e - 1]]
+
+from itertools import groupby
+
+def compact2(iterable):
+    """
+    This works for iterables
+    and returns a list
+    """
+    return [k for k,_ in groupby(iterable)]
+
+
+def compact(iterable):
+    """
+    This works for iterables
+    and return an iterable
+    """
+    return (key for key, _ in groupby(iterable))
