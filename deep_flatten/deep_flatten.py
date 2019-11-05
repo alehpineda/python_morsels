42b9fb5f0803055e665696b1f046e6f200fc8f05
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-04 20:09:01 -0600

    Update deep_flatten.py
    Updated the if, elif stament to a one liner.
    Added comments explaining whats happening in the program.

--- deep_flatten/deep_flatten.py
+++ deep_flatten/deep_flatten.py
@@ -3,12 +3,13 @@ from collections.abc import Iterable
 # Write a function that returns a flattened version of that list.
 
 def deep_flatten(lst):
+    # For each element in the list
     for element in lst:
-        if isinstance(element, str):
-            yield element
-             
-        elif isinstance(element, Iterable):
+        # If the element is instance of Iterable and not instance of string
+        if isinstance(element, Iterable) and not isinstance(element, str):
+            # Yield the element recursively from deep_flatten 
             yield from deep_flatten(element)
 
         else:
+            # Else yield the element
             yield element
