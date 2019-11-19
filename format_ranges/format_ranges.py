1ff1b8797ddb3afa86fbd804113b0ed916b6f4f5
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-19 13:06:26 -0600

    format_ranges -> Done
    Done Bonus 3.

--- format_ranges/format_ranges.py
+++ format_ranges/format_ranges.py
@@ -5,41 +5,24 @@ def format_ranges(list_numbers):
     num_groups = []
     counter = count()
 
-    #print(*_unique_ranges(list_numbers))
-
     for unique_range in _unique_ranges(list_numbers):
-        #print(unique_range)
+
         for _, group in groupby(sorted(unique_range), lambda x:x-next(counter)):
             group = list(group)
-            #print(f'lista: {sorted(list_numbers)}')
-            #print(f'group: {group}')
-            #print(f'diff: {[x for x in sorted(list_numbers) if x not in set(group)]}')
+
             if len(group) > 1:
-                #print(f'TRUE: {group[0]}-{group[-1]}')
-                num_groups.append(f'{group[0]}-{group[-1]}')
+                num_groups.append((group[0], group[-1]))
             
             else:
-                #print(f'FALSE: {group[0]}')
-                num_groups.append(f'{group[0]}')
+                num_groups.append((group[0],))
     
-    #print(num_groups)
-    #print(sorted(num_groups))
+    num_groups.sort()
 
-    return ','.join(num_groups)
+    return ','.join([f'{r[0]}-{r[-1]}' if len(r) > 1 else f'{r[0]}' for r in num_groups])
 
 def _unique_ranges(list_numbers):
     count = Counter(list_numbers)
-    #sets = []
-    #print(list(set(count.elements())), len(set(count.elements())))
-    while len(set(count.elements())) > 0:
-        #print(list(set(count.elements())))
-        yield list(set(count.elements()))
-        #sets.append(list(set(count.elements()) ) )
-        count.subtract(set(count.elements()))
-    
-    #return order_sets(sets)
 
-def order_sets(set):
-    # order the set by the minimun value of the set and
-    # the minimum len
-    pass
+    while len(set(count.elements())) > 0:
+        yield list(set(count.elements()))
+        count.subtract(set(count.elements()))
