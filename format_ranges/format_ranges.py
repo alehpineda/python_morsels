64c6a1af79cd46bd14ccf6260b8f64b667334363
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-11-18 21:55:41 -0600

    format_ranges -> Base, B1, and B2 done

---
+++ format_ranges/format_ranges.py
@@ -0,0 +1,36 @@
+from itertools import groupby, count
+from collections import Counter
+
+def format_ranges(list_numbers):
+    num_groups = []
+    counter = count()
+
+    #print(*_unique_ranges(list_numbers))
+
+    for unique_range in _unique_ranges(list_numbers):
+        #print(unique_range)
+        for _, group in groupby(sorted(unique_range), lambda x:x-next(counter)):
+            group = list(group)
+            #print(f'lista: {sorted(list_numbers)}')
+            #print(f'group: {group}')
+            #print(f'diff: {[x for x in sorted(list_numbers) if x not in set(group)]}')
+            if len(group) > 1:
+                #print(f'TRUE: {group[0]}-{group[-1]}')
+                num_groups.append(f'{group[0]}-{group[-1]}')
+            
+            else:
+                #print(f'FALSE: {group[0]}')
+                num_groups.append(f'{group[0]}')
+    
+    #print(num_groups)
+    #print(sorted(num_groups))
+
+    return ','.join(num_groups)
+
+def _unique_ranges(list_numbers):
+    count = Counter(list_numbers)
+    #print(list(set(count.elements())), len(set(count.elements())))
+    while len(set(count.elements())) > 0:
+        #print(list(set(count.elements())))
+        yield list(set(count.elements()))
+        count.subtract(set(count.elements()))
