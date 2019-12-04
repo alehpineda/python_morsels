5ba42f9fe69df901453d65f8432f230683b1566f
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-12-03 21:21:05 -0500

    orderedset - Done

--- orderedset/orderedset.py
+++ orderedset/orderedset.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
+from collections.abc import MutableSet
 
 class OrderedSet(object):
     def __init__(self, lista):
-        self.lista = lista
+        self.lista = OrderedDict.fromkeys(lista)
+        #self.lista = MutableSet(lista)
     
     
     def __iter__(self):
@@ -16,3 +18,27 @@ class OrderedSet(object):
     
     def __contains__(self, key):
         return key in self.lista
+
+
+    # Bonus 1 - add and discard
+    def add(self, palabra):
+        self.lista.__setitem__(palabra, palabra)
+
+
+    def discard(self, palabra):
+        try:
+            del self.lista[palabra]
+        except KeyError:
+            pass
+
+
+    # Bonus 2 - Equality
+    def __eq__(self, other):
+        if not isinstance(other, OrderedSet):
+            return self.lista.keys()==other
+        
+        return list(self.lista.keys())==list(other.lista.keys())
+
+    # Bonus 3 - Supports index
+    def __getitem__(self, i):
+        return list(self.lista.items())[i][0]
