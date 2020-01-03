d5f0f71408a76d0e8e1c975ba47eec15dbc53eaa
Alejandro H Pineda <ale.hpineda@gmail.com>
2020-01-03 12:50:55 -0500

    meetup_date -> Done
    Used dateutil for this exercise

---
+++ meetup_date/meetup_date.py
@@ -0,0 +1,32 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import SU, MO, TU, WE, TH, FR, SA
+from calendar import monthrange
+
+
+class Weekday(object):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+
+WEEKDAYS = {
+    0: MO,
+    1: TU,
+    2: WE,
+    3: TH,
+    4: FR,
+    5: SA,
+    6: SU
+}
+
+
+def meetup_date(year, month, *, nth=4, weekday=Weekday.THURSDAY):
+    if nth < 0:
+        return date(year, month, monthrange(year, month)[1]) + relativedelta(
+            weekday=WEEKDAYS[weekday](nth))
+    return date(year, month, 1) + relativedelta(weekday=WEEKDAYS[weekday](nth))
