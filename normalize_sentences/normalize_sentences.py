2101af39fec2d3f44f14aabc333d0a464371fac9
Alejandro Pineda <ale.hpineda@gmail.com>
2020-02-09 23:27:48 -0600

    Update normalize_sentences.py
    Reduced capturing groups from 2 to 1

--- normalize_sentences/normalize_sentences.py
+++ normalize_sentences/normalize_sentences.py
@@ -1,6 +1,7 @@
 import re
 
 
-# mut.py --target normalize_sentences --unit-test test_normalize_sentences --runner pytest --show-mutant --coverage --colored-output
+# mut.py --target normalize_sentences --unit-test test_normalize_sentences
+# --runner pytest --show-mutant --coverage --colored-output
 def normalize_sentences(text):
-    return re.sub(r'([\S][^\.]\S)([?.!]) +', r'\1\2  ', text)
+    return re.sub(r'([\S][^\.]\S[?.!]) +', r'\1  ', text)
