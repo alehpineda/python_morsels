20de2a037db8cf313439d2f55b5c58b538dd4269
Alejandro Pineda <ale.hpineda@gmail.com>
2020-02-11 21:50:35 -0600

    mutpy changes

--- suppress/suppress.py
+++ suppress/suppress.py
@@ -1,23 +1,7 @@
-from contextlib import ContextDecorator
 from contextlib import contextmanager
-
-class suppress1(ContextDecorator):
-    def __init__(self, *exception_types):
-        self.exception_types = exception_types
-    
-
-    def __enter__(self):
-        return self
-
-
-    def __exit__(self, exception_type, exception, traceback):
-        self.exception = exception
-        self.traceback = traceback
-        return isinstance(exception, self.exception_types)
 
 
 # Solution using contexmanager
-
 class ExceptionInfo(object):
     exception = None
     traceback = None
