6ffa534ae12eb8b1501dcdde1b0b0fef67a91ee3
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-03 14:05:53 -0600

    added mutpy to the workflow

---
+++ mut.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+python_files = os.popen('find . -name "*.py" -and -not -name "test_*.py" -and -not -name "mut.py"')
+python_output = python_files.read()
+
+test_files = os.popen('find . -name "test_*.py" -and -not -name "mut.py"')
+test_output = test_files.read()
+
+for files, tests in zip(sorted(python_output.strip().split('\n')),
+                        sorted(test_output.strip().split('\n'))):
+    print(f'mut.py --target {files} --unit-test {tests} --runner pytest --coverage')
+    # os.system(f'mut.py --target {files} --unit-test {tests} --runner pytest --coverage')
+
+    process = subprocess.Popen(['mut.py',
+                                f'--target {files}',
+                                f'--unit-test {tests}',
+                                '--runner pytest',
+                                '--coverage'],
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+
+    while True:
+        output = process.stdout.readline()
+        print(output.strip())
+        # Do something else
+        return_code = process.poll()
+        if return_code is not None:
+            print('RETURN CODE', return_code)
+            # Process has finished, read rest of the output
+            for output in process.stdout.readlines():
+                print(output.strip())
+            break
--- orderedset/orderedset.py
+++ orderedset/orderedset.py
@@ -1,4 +1,4 @@
-from collections.abc import MutableSet
+from collections.abc import MutableSet, Sequence
 
 
 class OrderedSet(MutableSet):
@@ -32,3 +32,46 @@ class OrderedSet(MutableSet):
     # Bonus 3 - Supports index
     def __getitem__(self, i):
         return list(self.items.items())[i][0]
+
+
+class OrderedSet2(Sequence, MutableSet):
+
+    """Set-like object that maintains insertion order of items."""
+
+    def __init__(self, iterable):
+        self.items = set()
+        self.order = []
+        self |= iterable
+
+
+    def __contains__(self, item):
+        return item in self.items
+
+
+    def __len__(self):
+        return len(self.items)
+
+
+    def __getitem__(self, index):
+        return self.order[index]
+
+
+    def add(self, item):
+        if item not in self.items:
+            self.order.append(item)
+        self.items.add(item)
+
+
+    def discard(self, item):
+        if item in self.items:
+            self.order.remove(item)
+            self.items.remove(item)
+
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return (
+                len(self) == len(other) and
+                all(x == y for x, y in zip(self, other))
+            )
+        return super().__eq__(other)
