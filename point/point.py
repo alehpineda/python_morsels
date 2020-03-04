f2fe18ee8ee395089dce6cba92b1fada42dbe022
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-03 21:45:00 -0600

    flake8

--- point/point.py
+++ point/point.py
@@ -1,17 +1,14 @@
-# Point class
-
 class Point:
     # Attributes
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-    
+
     # String representation
     def __str__(self):
         return f'Point(x={self.x}, y={self.y}, z={self.z})'
-    
-    
+
     def __repr__(self):
         return f'Point(x={self.x}, y={self.y}, z={self.z})'
 
@@ -21,7 +18,6 @@ class Point:
         x2, y2, z2 = other
         return (x1, y1, z1) == (x2, y2, z2)
 
-
     # Bonus 1
     # Allow objects to be added and substracted
     def __add__(self, other):
@@ -30,13 +26,11 @@ class Point:
 
         return Point(x1 + x2, y1 + y2, z1 + z2)
 
-    
     def __sub__(self, other):
         x1, y1, z1 = self
         x2, y2, z2 = other
 
         return Point(x1 - x2, y1 - y2, z1 - z2)
-
 
     # Bonus 2
     # Allow objects to be scaled up and down by numbers
@@ -45,11 +39,9 @@ class Point:
         x, y, z = self
         return Point(x * scalar, y * scalar, z * scalar)
 
-
     def __rmul__(self, scalar):
         # int * obj
         return self.__mul__(scalar)
-
 
     # Bonus 3
     # Allow point objects to be unpacked
