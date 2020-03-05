87ae0a525bbfc35286bd4d6d7e0c3818d65fe68f
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-04 21:13:19 -0600

    pylint

--- add/add.py
+++ add/add.py
@@ -1,8 +1,14 @@
+""" add function """
 from typing import List
 from itertools import zip_longest
 
 
 def add(*args: List) -> List:
+    """
+        Function that accepts two lists-of-lists of numbers and returns one
+        list-of-lists with each of the corresponding numbers in the two given
+        lists-of-lists added together.
+    """
     try:
         return [list(map(sum, zip_longest(*t))) for t in zip_longest(*args)]
     except TypeError:
--- add/test_add.py
+++ add/test_add.py
@@ -1,3 +1,4 @@
+""" Tests for add. """
 from copy import deepcopy
 import unittest
 
@@ -6,57 +7,64 @@ from add import add
 
 class AddTests(unittest.TestCase):
 
-    """Tests for add."""
+    """ Tests for add. """
 
     def test_single_items(self):
+        """ Tests for single items """
         self.assertEqual(add([[5]], [[-2]]), [[3]])
 
     def test_two_by_two_matrixes(self):
-        m1 = [[6, 6], [3, 1]]
-        m2 = [[1, 2], [3, 4]]
-        m3 = [[7, 8], [6, 5]]
-        self.assertEqual(add(m1, m2), m3)
+        """ Tests for two by two matrixes """
+        matrix_1 = [[6, 6], [3, 1]]
+        matrix_2 = [[1, 2], [3, 4]]
+        matrix_3 = [[7, 8], [6, 5]]
+        self.assertEqual(add(matrix_1, matrix_2), matrix_3)
 
     def test_two_by_three_matrixes(self):
-        m1 = [[1, 2, 3], [4, 5, 6]]
-        m2 = [[-1, -2, -3], [-4, -5, -6]]
-        m3 = [[0, 0, 0], [0, 0, 0]]
-        self.assertEqual(add(m1, m2), m3)
+        """ Tests for two by three matrixes """
+        matrix_1 = [[1, 2, 3], [4, 5, 6]]
+        matrix_2 = [[-1, -2, -3], [-4, -5, -6]]
+        matrix_3 = [[0, 0, 0], [0, 0, 0]]
+        self.assertEqual(add(matrix_1, matrix_2), matrix_3)
 
     def test_input_unchanged(self):
-        m1 = [[6, 6], [3, 1]]
-        m2 = [[1, 2], [3, 4]]
-        m1_original = deepcopy(m1)
-        m2_original = deepcopy(m2)
-        add(m1, m2)
-        self.assertEqual(m1, m1_original)
-        self.assertEqual(m2, m2_original)
+        """ Tests for input unchanged """
+        matrix_1 = [[6, 6], [3, 1]]
+        matrix_2 = [[1, 2], [3, 4]]
+        matrix_1_original = deepcopy(matrix_1)
+        matrix_2_original = deepcopy(matrix_2)
+        add(matrix_1, matrix_2)
+        self.assertEqual(matrix_1, matrix_1_original)
+        self.assertEqual(matrix_2, matrix_2_original)
 
     # To test the Bonus part of this exercise, comment out the following line
     # @unittest.expectedFailure
     def test_any_number_of_matrixes(self):
-        m1 = [[6, 6], [3, 1]]
-        m2 = [[1, 2], [3, 4]]
-        m3 = [[2, 1], [3, 4]]
-        m4 = [[9, 9], [9, 9]]
-        m5 = [[31, 32], [27, 24]]
-        self.assertEqual(add(m1, m2, m3), m4)
-        self.assertEqual(add(m2, m3, m1, m1, m2, m4, m1), m5)
+        """ Tests for any number of matrixes """
+        matrix_1 = [[6, 6], [3, 1]]
+        matrix_2 = [[1, 2], [3, 4]]
+        matrix_3 = [[2, 1], [3, 4]]
+        matrix_4 = [[9, 9], [9, 9]]
+        matrix_5 = [[31, 32], [27, 24]]
+        self.assertEqual(add(matrix_1, matrix_2, matrix_3), matrix_4)
+        self.assertEqual(add(matrix_2, matrix_3, matrix_1, matrix_1, matrix_2,
+                             matrix_4, matrix_1), matrix_5)
 
     # To test the Bonus part of this exercise, comment out the following line
     # @unittest.expectedFailure
     def test_different_matrix_size(self):
-        m1 = [[6, 6], [3, 1]]
-        m2 = [[1, 2], [3, 4], [5, 6]]
-        m3 = [[6, 6], [3, 1, 2]]
+        """ Tests for different matrix size """
+        matrix_1 = [[6, 6], [3, 1]]
+        matrix_2 = [[1, 2], [3, 4], [5, 6]]
+        matrix_3 = [[6, 6], [3, 1, 2]]
         with self.assertRaises(ValueError):
-            add(m1, m2)
+            add(matrix_1, matrix_2)
         with self.assertRaises(ValueError):
-            add(m1, m3)
+            add(matrix_1, matrix_3)
         with self.assertRaises(ValueError):
-            add(m1, m1, m1, m3, m1, m1)
+            add(matrix_1, matrix_1, matrix_1, matrix_3, matrix_1, matrix_1)
         with self.assertRaises(ValueError):
-            add(m1, m1, m1, m2, m1, m1)
+            add(matrix_1, matrix_1, matrix_1, matrix_2, matrix_1, matrix_1)
 
 
 if __name__ == "__main__":
