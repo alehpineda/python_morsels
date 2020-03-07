ae2366a54b01847f6cd35a5563f677ed0773e712
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-07 10:48:54 -0600

    Inheriting from UserDict

--- easy_dict/easy_dict.py
+++ easy_dict/easy_dict.py
@@ -1,29 +1,22 @@
-class EasyDict(object):
+from collections import UserDict
+
+
+class EasyDict(UserDict):
+
     def __init__(self, _dict={}, normalize=False, **kwargs):
-
         self._normalize = normalize
+        self.update(_dict)
+        self.update(kwargs)
 
-        for key, value in _dict.items():
-            self[key] = value
-        for key, value in kwargs.items():
-            self[key] = value
+    @property
+    def data(self):
+        return self.__dict__
 
     def __getitem__(self, key):
-        return getattr(self, self.normalized(key))
+        return self.__dict__[self.normalized(key)]
 
     def __setitem__(self, key, value):
-        setattr(self, self.normalized(key), value)
-
-    def get(self, key, default=None):
-        return getattr(self, self.normalized(key), default)
-
-    def __eq__(self, other):
-        if isinstance(other, EasyDict):
-            return self.__dict__ == other.__dict__
-        return NotImplemented
+        self.__dict__[self.normalized(key)] = value
 
     def normalized(self, key):
-        if self._normalize:
-            return key.replace(' ', '_')
-        else:
-            return key
+        return key.replace(' ', '_') if self._normalize else key
