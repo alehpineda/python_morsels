b7a0b5eb24e1c86a75a63010c20264c8f76c458b
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-11 15:18:01 -0500

    bonus 1,2,3

--- condense_csv/condense_csv.py
+++ condense_csv/condense_csv.py
@@ -1,12 +1,27 @@
-from collections import defaultdict
+from io import StringIO
+from itertools import groupby
+from operator import itemgetter
+import csv
 
 
-def condense_csv(csv_text, id_name):
-    groups = defaultdict(dict)
-    for row in csv_text.splitlines():
-        _id, _attr, _value = row.split(',')
-        groups[_id][_attr] = _value
+def condense_csv(csv_text, id_name=None):
+    groups = []
+    csv_reader = csv.reader(csv_text.splitlines())
 
-    headers = [id_name, *groups[_id].keys()]
-    rows = [[_id, *_attr.values()] for _id, _attr in groups.items()]
-    return '\n'.join(','.join(row) for row in [headers, *rows])
+    if id_name is None:
+        [id_name, *other_headers] = next(csv_reader)
+    attrs = [id_name]
+    for _id, _rows in groupby(csv_reader, key=itemgetter(0)):
+        group = {
+            _attr: _value
+            for _id, _attr, _value in _rows
+        }
+        group[id_name] = _id
+        groups.append(group)
+        attrs.extend(group.keys())
+    out_file = StringIO()
+    headers = dict.fromkeys(attrs, None).keys()
+    writer = csv.DictWriter(out_file, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(groups)
+    return out_file.getvalue()
