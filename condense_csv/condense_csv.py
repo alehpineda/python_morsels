2e05034d174704a7897c4b012cbf2997cdd79755
Alejandro Pineda <ale.hpineda@gmail.com>
2020-03-11 15:19:45 -0500

    added spaces

--- condense_csv/condense_csv.py
+++ condense_csv/condense_csv.py
@@ -10,18 +10,19 @@ def condense_csv(csv_text, id_name=None):
 
     if id_name is None:
         [id_name, *other_headers] = next(csv_reader)
+
     attrs = [id_name]
+
     for _id, _rows in groupby(csv_reader, key=itemgetter(0)):
-        group = {
-            _attr: _value
-            for _id, _attr, _value in _rows
-        }
+        group = {_attr: _value for _id, _attr, _value in _rows}
         group[id_name] = _id
         groups.append(group)
         attrs.extend(group.keys())
+
     out_file = StringIO()
     headers = dict.fromkeys(attrs, None).keys()
     writer = csv.DictWriter(out_file, fieldnames=headers)
     writer.writeheader()
     writer.writerows(groups)
+
     return out_file.getvalue()
